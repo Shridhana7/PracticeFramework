68174c86aa389a600a56bbaeddd98851938a81df
10809 <10809@NG0FGR93.ardianet.net>
2024-01-24 11:35:08 +0530

    first commit

---
+++ src/test/java/Loop_Program.java
@@ -0,0 +1,27 @@
+
+public class Loop_Program {
+	
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+ 
+			for (int i = 1; i<=5; i++)
+			{
+				System.out.println(i + "Hello World");
+			}
+				int i = 10;
+				while (i >= -10)
+				{
+				System.out.println(i);
+				i--;
+				}
+				do
+				{
+					System.out.println(i);
+					i++;
+				}
+				while(i <=10);
+
+				}
+		}
+ 
+
